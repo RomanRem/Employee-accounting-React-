9d50ea2ebc0f7c21a7e66026087bc3bd68b7594f
RomanRem <romanbabiychuk@gmail.com>
2021-10-22 21:38:55 +0500

    добавление данных из базы заглушки в App.jsx

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,13 @@ import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
  function App(){
+        const data =[ 
+        {name: "Roman.B", salary: 800},
+        {name: "Elena.B", salary: 300},
+        {name: "John.N", salary: 700},
+        {name: "Anastasia.B", salary: 10000},
+        ];
+
      return (
          <div className='app'>
              <AppInfo/>
@@ -15,7 +22,7 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
                 <SearchPanel/>
                 <AppFilter/>
              </div>
-                <EmployeesList/>
+                <EmployeesList data={data}/>
                 <EmployeesAddForm/>
          </div>
      );
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './employees-list.css';
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 
-const EmployeesList =()=>{
+const EmployeesList =({data})=>{
+
+    const elements = data.map(item =>{
+        return(
+            <EmployeesListItem {...item}/>   //Object spread operator вместо 
+                                             //name={item.name} salary={item.salary}
+        )
+    })
 
     return(
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
 
         </ul>
     )
